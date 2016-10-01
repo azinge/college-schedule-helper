b0f0125484eb2911ed9a07a6f634bb59e7dfd199
cazinge <eddie.azinge@gmail.com>
2016-10-01 08:59:37 -0700

    Changed PDF naming convention

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var output = document.getElementById('output');
-PDFJS.getDocument('../pdf/Fall 2016.pdf').then(function (pdf) {
+var scheduleName = 'Fall_2016';
+PDFJS.getDocument('https://rawgit.com/cazinge/LMU-Schedule-Helper/master/assets/pdf/' + scheduleName + '.pdf').then(function (pdf) {
     pdf.getPage(1).then(function (page) {
         page.getTextContent().then(function (textContent) {
             output.innerHTML = textContent.items.map(x=>x.str).join(" | ");
